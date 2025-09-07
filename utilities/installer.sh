e436b0cb73fab2ab644e2640dc3f2fdc1c618298
ennwerd <publicthreat@proton.me>
2025-09-07 02:20:16 -0500

    added distro-specific installer

--- utilities/installer.sh
+++ utilities/installer.sh
@@ -3,34 +3,34 @@
 # Directories.
 REQUIREMENTS_FILE=$PROJECT_DIR/requirements.txt
 
-# Installing relevant debian packages
-# TODO: implement this
+# Distro-specific installs
+./distro_specific_installer.sh
 
 # Make Python venv if it doesn't exist
 if [[ ! -d $VENV_DIR ]]; then
-  echo "Making venv"
-  python3 -m venv $VENV_DIR
+	echo "Making venv"
+	python3 -m venv $VENV_DIR
 fi
 
-# Activate venv
+Activate venv
 source $VENV_DIR/bin/activate
 
 # Install requirements
 echo "Installing from 'requirements.txt'"
 if [ -e $REQUIREMENTS_FILE ]; then
-  pip install -r $REQUIREMENTS_FILE
+	pip install -r $REQUIREMENTS_FILE
 else
-  echo "No 'requirements.txt' found"
-  echo "Download 'requirements.txt' from:"
-  echo "https://github.com/wordboxx/autoSpotDL.git"
-  exit 1
+	echo "No 'requirements.txt' found"
+	echo "Download 'requirements.txt' from:"
+	echo "https://github.com/wordboxx/autoSpotDL.git"
+	exit 1
 fi
 echo "Installation complete"
 
 # Install ffmpeg if it's not installed
 if ! command -v ffmpeg &>/dev/null; then
-  echo "Installing ffmpeg"
-  spotdl --download-ffmpeg
+	echo "Installing ffmpeg"
+	spotdl --download-ffmpeg
 fi
 
 # Deactivate venv
@@ -38,5 +38,5 @@ deactivate
 
 # DL_DIR Management
 if [[ ! -d $DL_DIR ]]; then
-  mkdir $DL_DIR
+	mkdir $DL_DIR
 fi
